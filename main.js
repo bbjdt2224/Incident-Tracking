be7d7c9d7812777bf1607eafc9f8b0c3ddd36826
Justin True <justin.d.true@wmich.edu>
2018-02-25 13:58:07 -0500

    added more routes and queries to database

--- main.js
+++ main.js
@@ -4,38 +4,110 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 var db = require('./models/index.js');
-//res.send('Hello World')
 //GET /incidents 
 app.get('/incidents', function (req, res) {
+    var incidentarr = [];
     db.incidents.findAll({
         where: {
             userId: 1
         }
     }).then(function (incidents) {
-        incidents.forEach(function (element) {
-            console.log(element.id);
+        incidents.forEach(function (incident) {
+            db.incidentrevisions.findOne({
+                where: {
+                    incidentId: incident.id,
+                    revisionNumber: incident.revisionId
+                }
+            }).then(function (revision) {
+                incidentarr.push(revision.shortDescription);
+            });
         });
     });
+    res.send(incidentarr);
 });
 //POST /incidents
 app.post('/incidents', function (req, res) {
-    var i = db.incidents.build({
-        revisionId: req.body.revisionId,
+    db.incidents.create({
+        revisionId: 1,
         userId: req.body.userId,
         trackerId: req.body.trackerId
     });
-    i.save();
-    var ir = db.incidentrevisions.build({
+    db.incidentrevisions.create({
         incidentId: req.body.incidentId,
-        revisionNumebr: req.body.revisionNumber,
-        timestamp: req.body.timestamp,
+        revisionNumber: '1',
         type: req.body.type,
         shortDescription: req.body.shortDescription,
-        longDescription: req.body.longDescription,
-        resolution: req.body.resolution,
-        severity: req.body.severity
+        longDescription: req.body.longDescription
     });
-    ir.save();
+    res.send("Done");
+});
+//GET /incident/(id)
+app.get('/incident/:id', function (req, res) {
+    db.incidents.findById(req.params['id']).then(function (incident) {
+        db.incidentrevisions.findOne({
+            where: {
+                incidentId: incident.id,
+                revisionNumber: incident.revisionId
+            }
+        }).then(function (revision) {
+            res.send(revision);
+        });
+    });
+});
+app.put('/incident/:id', function (req, res) {
+    var pastRevision;
+    db.incidents.findById(req.params['id']).then(function (incident) {
+        db.incidentrevisions.findOne({
+            where: {
+                incidentId: incident.id,
+                revisionNumber: incident.revisionId
+            }
+        }).then(function (revision) {
+            pastRevision = revision;
+        });
+    });
+    db.incidentrevisions.create({
+        revisionNumber: pastRevision.revisionNumber + 1,
+        type: req.body.type,
+        shortDescription: req.body.shortDescription,
+        longDescription: req.body.longDescription
+    });
+    db.incidents.update({
+        revisionId: pastRevision.revisionNumber + 1,
+        trackerId: req.body.trackerId
+    }, {
+        where: {
+            id: req.params['id']
+        }
+    });
+});
+app.post('/signup', function (req, res) {
+    db.users.create({
+        email: req.body.email
+        //password
+    });
+});
+app.post('/login', function (req, res) {
+    db.users.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(function (user) {
+        //check if password is same as in database
+    });
+});
+app.post('/logout', function (req, res) {
+});
+app.put('/incident/:id', function (req, res) {
+    db.users.update({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        role: req.body.role
+    }, {
+        where: {
+            id: req.params['id']
+        }
+    });
 });
 app.listen(3000, function () {
     console.log("Server Started localhost:3000");
--- main.ts
+++ main.ts
@@ -7,43 +7,121 @@ app.use(bodyParser.json());
 
 var db = require('./models/index.js')
 
-//res.send('Hello World')
-
 //GET /incidents 
 app.get('/incidents', function (req, res) {
+  var incidentarr = [];
   db.incidents.findAll({
     where: {
       userId: 1
     }
   }).then(incidents =>{
-    incidents.forEach(element => {
-      console.log(element.id)
+    incidents.forEach(incident => {
+      db.incidentrevisions.findOne({
+        where: {
+          incidentId: incident.id,
+          revisionNumber: incident.revisionId
+        }
+      }).then(revision =>{
+        incidentarr.push(revision.shortDescription)
+      })
     });
   })
+  res.send(incidentarr)
 })
  
 //POST /incidents
 app.post('/incidents', function (req, res) {
-  const i = db.incidents.build({
-    revisionId: req.body.revisionId,
+  db.incidents.create({
+    revisionId: 1,
     userId: req.body.userId,
     trackerId: req.body.trackerId
   })
-  i.save()
-  const ir = db.incidentrevisions.build({
+  db.incidentrevisions.create({
     incidentId: req.body.incidentId,
-    revisionNumebr: req.body.revisionNumber,
-    timestamp: req.body.timestamp,
+    revisionNumber: '1',
     type: req.body.type,
     shortDescription: req.body.shortDescription,
-    longDescription: req.body.longDescription,
-    resolution: req.body.resolution,
-    severity: req.body.severity
+    longDescription: req.body.longDescription
   })
-  ir.save()
+  res.send("Done")
 })
 
+//GET /incident/(id)
+app.get('/incident/:id', function(req, res) {
+  db.incidents.findById(req.params['id']).then(incident => {
+    db.incidentrevisions.findOne({
+      where: {
+        incidentId: incident.id,
+        revisionNumber: incident.revisionId
+      }
+    }).then(revision => {
+      res.send(revision)
+    })
+  })
+})
 
+app.put('/incident/:id', function(req, res) {
+  var pastRevision;
+  db.incidents.findById(req.params['id']).then(incident => {
+    db.incidentrevisions.findOne({
+      where: {
+        incidentId: incident.id,
+        revisionNumber: incident.revisionId
+      }
+    }).then(revision => {
+      pastRevision = revision
+    })
+  })
+
+  db.incidentrevisions.create({
+    revisionNumber: pastRevision.revisionNumber+1,
+    type: req.body.type,
+    shortDescription: req.body.shortDescription,
+    longDescription: req.body.longDescription
+  })
+
+  db.incidents.update({
+    revisionId: pastRevision.revisionNumber+1,
+    trackerId: req.body.trackerId
+  },{
+    where: {
+      id: req.params['id']
+    }
+  })
+})
+
+app.post('/signup', function (req, res) {
+  db.users.create({
+    email: req.body.email
+    //password
+  })
+})
+
+app.post('/login', function (req, res) {
+  db.users.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(user => {
+      //check if password is same as in database
+  })
+})
+
+app.post('/logout', function (req, res) {
+  
+})
+
+app.put('/incident/:id', function(req, res) {
+  db.users.update({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    role: req.body.role
+  }, {
+    where: {
+      id: req.params['id']
+    }
+  })
+})
 
 app.listen(3000, function(){
   console.log("Server Started localhost:3000");
